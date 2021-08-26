c9f24e386837171e874c527501a90470f517d4c6
Paul Barcenas <paulbarcenas@yahoo.com>
2021-08-26 00:45:50 -0500

    load the task into form for edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var pageContentEl = document.querySelector("#page-content");
+var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
@@ -24,19 +26,99 @@ var taskFormHandler = function(event) {
 
 };
 var  createTaskEl =function(taskDataObj) {
-    // create list item
 var listItemEl = document.createElement("li");
 listItemEl.className = "task-item";
 
-// create div to hold task info and add to list item
+// add task id as a custom attribute
+listItemEl.setAttribute("data-task-id", taskIdCounter);
+
 var taskInfoEl = document.createElement("div");
 taskInfoEl.className = "task-info";
 taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 listItemEl.appendChild(taskInfoEl);
 
+var taskActionsEl = createTaskActions(taskIdCounter);
+listItemEl.appendChild(taskActionsEl);
 // add entire list item to list
 tasksToDoEl.appendChild(listItemEl);
+
+//increase task counter for next unique id
+taskIdCounter++;
+};
+
+var createTaskActions = function(taskId) {
+    var actionContainerEl = document.createElement("div");
+    actionContainerEl.className = "task-actions";
+    // create edit button
+    var editButtonEl = document.createElement("button");
+    editButtonEl.textContent = "Edit";
+    editButtonEl.className = "btn edit-btn";
+    editButtonEl.setAttribute("data-task-id", taskId);
+
+    actionContainerEl.appendChild(editButtonEl);
+
+    // create delete button
+    var deleteButtonEl= document.createElement("button");
+    deleteButtonEl.textContent = "delete";
+    deleteButtonEl.className = "btn delete-btn";
+    deleteButtonEl.setAttribute("data-task-id", taskId);
+
+    actionContainerEl.appendChild(deleteButtonEl);
+    var statusSelectEl = document.createElement("select");
+    statusSelectEl.className = "select-status";
+    statusSelectEl.setAttribute("name", "status-change");
+    statusSelectEl.setAttribute("data-task-id", taskId);
+    
+    actionContainerEl.appendChild(statusSelectEl);
+
+    var statusChoices = ["To Do", "In Progress", "Completed"];
+    for (var i= 0; i < statusChoices.length; i++) {
+        // creat option element
+        var statusOptionEl = document.createElement("option");
+        statusOptionEl.textContent = statusChoices[i];
+        statusOptionEl.setAttribute("value", statusChoices[i]);
+
+        // apend to select
+        statusSelectEl.appendChild(statusOptionEl);
+    }
+    return actionContainerEl;
 };
 
 formEl.addEventListener("submit", taskFormHandler);
 
+var taskButtonHandler = function(event) {
+    // target element from event
+    var targetEl = event.target;
+
+    // edit button was clicked
+    if (targetEl.matches(".edit-btn")) {
+        var taskId = targetEl.getAttribute("data-task-id");
+        editTask(taskId);
+    }
+    // delte button was clicked
+    else if (targetEl.matches(".delete-btn")){
+        var taskId = targetEl.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+};
+
+var editTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    // get content from task name and type
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+    console.log(taskName);
+
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+    document.querySelector("input[name='task-name']").value = taskName;
+    document.querySelector("select[name='task-type']").value =taskType;
+    document.querySelector("#save-task").textContent = "Save Task";
+    formEl.setAttribute("data-task-id", taskId);
+
+};
+pageContentEl.addEventListener("click", taskButtonHandler);
